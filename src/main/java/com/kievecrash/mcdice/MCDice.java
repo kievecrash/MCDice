c958efce23bc3cf82b0c6c0abb28f8a33724a84b
Andy Lukas <kievecrash@gmail.com>
2014-07-01 16:24:59 -0400

    basic setup/initial commit

---
+++ src/main/java/com/kievecrash/mcdice/MCDice.java
@@ -0,0 +1,28 @@
+package com.kievecrash.mcdice;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "MCDice", name = "MCDice", version = "1.7.2-1.0")
+public class MCDice{
+
+    @Mod.Instance("MCDice")
+    public static MCDice instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
