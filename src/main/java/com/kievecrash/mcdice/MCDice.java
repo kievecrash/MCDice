f92a2016fb532b2416bba0d3077549fe0d59f470
Andy Lukas <kievecrash@gmail.com>
2014-07-01 18:00:23 -0400

    Slight tweaks in naming, added mcmod.info

--- src/main/java/com/kievecrash/mcdice/MCDice.java
+++ src/main/java/com/kievecrash/mcdice/MCDice.java
@@ -5,7 +5,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "MCDice", name = "MCDice", version = "1.7.2-1.0")
+@Mod(modid = "MCDice", name = "MC Dice", version = "1.7.2-1.0")
 public class MCDice{
 
     @Mod.Instance("MCDice")
